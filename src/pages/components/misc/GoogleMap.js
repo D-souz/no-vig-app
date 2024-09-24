c275b3db5cb3538c17408b86b304b97a03777eb1
D-souz <dhesouzanorbert36@gmail.com>
2024-09-24 19:31:32 +0300

    Commented the Google Maps API

--- src/pages/components/misc/GoogleMap.js
+++ src/pages/components/misc/GoogleMap.js
@@ -60,7 +60,7 @@ const GoogleMapPage = () => {
               </BlockDes>
             </BlockHeadContent>
           </BlockHead>
-          <PreviewCard>
+          {/* <PreviewCard>
             <Card className="card-bordered w-100 h-100">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19218.666257142526!2d-0.038921204804245685!3d52.97840720396935!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d7c2c53856f733%3A0x8406d541f1a0910c!2sBoston%2C%20UK!5e0!3m2!1sen!2sbd!4v1632376229328!5m2!1sen!2sbd"
@@ -71,8 +71,8 @@ const GoogleMapPage = () => {
                 title="google-map"
               ></iframe>
             </Card>
-          </PreviewCard>
-          <CodeBlock language="jsx">
+          </PreviewCard> */}
+          {/* <CodeBlock language="jsx">
             {` <Card className="card-bordered w-100 h-100">
   <iframe
     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19218.666257142526!2d-0.038921204804245685!3d52.97840720396935!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d7c2c53856f733%3A0x8406d541f1a0910c!2sBoston%2C%20UK!5e0!3m2!1sen!2sbd!4v1632376229328!5m2!1sen!2sbd"
@@ -82,7 +82,7 @@ const GoogleMapPage = () => {
     loading="lazy"
   ></iframe>
 </Card>`}
-          </CodeBlock>
+          </CodeBlock> */}
         </Block>
 
         <Block size="lg">
